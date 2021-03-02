0c86006aae98c816f566dc129cbb91a117a6503a
scott95100 <scott95100@gmail.com>
2021-03-02 16:32:01 -0700

    fixed capsules.js

--- models/capsule.js
+++ models/capsule.js
@@ -12,18 +12,4 @@ const capsuleSchema = new Schema({
 // Model
 const Capsule = mongoose.model('Capsule', capsuleSchema);
 
-const mongoose = require('mongoose');
-// Schema class from mongoose
-const Schema = mongoose.Schema;
-
-// Make a capsuleSchema
-const capsuleSchema = new Schema({
-    serial: { type: String, unique: true },
-    type: String,
-    waterLandings: Number,
-});
-
-// Model
-const Capsule = mongoose.model('Capsule', capsuleSchema);
-
 module.exports = Capsule;
