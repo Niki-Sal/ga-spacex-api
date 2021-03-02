4f902b06ac997838cdf0557cfb27b609f0a67ff7
mbrown <tugtuj@gmail.com>
2021-03-02 17:41:23 -0500

    commit

--- app.js
+++ app.js
@@ -1,17 +1,58 @@
-const express = require('express')
-const axios = require('axios')
+const express = require('express');
+const axios = require('axios');
+// Set up App
+const app = express();
+const PORT = process.env.PORT || 8000;
+// Database
+const db = require('./models');
+// Route
+app.get('/v1/', (req, res) => {
+    res.send('Welcome to GA Space X API');
+});
+app.get('/fetch-capsules', async (req, res) => {
+    // Run axios
+    const response = await axios.get('https://api.spacexdata.com/v4/capsules');
+    const data = response.data; // array of objects [{}, {}, {}]
+    // add each object info to DB
+    for (let i = 0; i < data.length; i++) {
+        let capsuleObject = data[i]; // object
+        const { serial, type, water_landings } = capsuleObject; // destructuring
+        db.Capsule.create({
+            serial: serial,
+            type: type,
+            waterLandings: water_landings
+        }, (err, newCapsule) => {
+            console.log(newCapsule);
+        })
+    }
+    res.json(data);
+});
+app.get('/fetch-capsules-again', async (req, res) => {
+    const response = await axios.get('https://api.spacexdata.com/v4/capsules');
+    const data = response.data; // array of objects [{}, {}, {}]
+    const newCapsules = await data.map((capsuleObject) => {
+        const { serial, type, water_landings } = capsuleObject; // destructuring
+    const resultObj = {
+        serial: serial,
+        type: type,
+        waterLandings: water_landings
+    }
+    return resultObj
+    });
+const allNewCapsules = await db.Capsule.create(newCapsules);
+res.json(allNewCapsules)
+});
 
-const app = express()
-const PORT = process.env.PORT || 8000
-
-axios.get('https://api.spacexdata.com/v4/capsules').then(response => {
-    console.log(response.data)
+app.get('v1/capsules', async (req, res)=> {
+    const fetchCapsules = await db.Capsule.find()
+    res.json(fetchCapsules)
 })
 
-app.get('/', (req, res) => {
-    res.send('Welcome to GA Space X API')
+app.get('/v1/capsules/:serial', async (req, res) => {
+    let { serial } = req.params
+    const fetchCapsule = await db.Capsule.find({ serial })
+    res.json(fetchCapsule);
 })
-
 const server = app.listen(PORT, () => {
-    console.log(`Server is listening on PORT: ${PORT}`)
-})
+    console.log(`Server is running on PORT: ${PORT}`);
+});
