d7432706b1c0f30285bd693fc840b9a5ab399045
Rome Bell <romebell5@gmail.com>
2021-03-02 12:47:18 -0800

    fetch-capsules: add route

--- app.js
+++ app.js
@@ -5,15 +5,34 @@ const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// Run axios
-axios.get('https://api.spacexdata.com/v4/capsules')
-.then(response => {
-    console.log(response.data);
-});
+// Database
+const db = require('./models');
+
 
 // Route
 app.get('/', (req, res) => {
     res.send('Welcome to GA Space X API');
+});
+
+app.get('/fetch-capsules', async (req, res) => {
+    // Run axios
+    const response = await axios.get('https://api.spacexdata.com/v4/capsules');
+    const data = response.data; // array of objects [{}, {}, {}]
+    // add each object info to DB
+    for (let i = 0; i < data.length; i++) {
+        let capsuleObject = data[i]; // object
+        const { serial, type, water_landings } = capsuleObject; // destructuring
+
+        db.Capsule.create({
+            serial: serial,
+            type: type,
+            waterLandings: water_landings
+        }, (err, newCapsule) => {
+            console.log(newCapsule);
+        })
+    }
+
+    res.json(data);
 });
 
 const server = app.listen(PORT, () => {
