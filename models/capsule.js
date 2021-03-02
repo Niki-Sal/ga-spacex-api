d52175cba14cce7e59f2ad8cfeea424ca907a9a8
mbrown <tugtuj@gmail.com>
2021-03-02 18:18:36 -0500

    ship add

--- models/capsule.js
+++ models/capsule.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-//Make a capsuleschema
-const capsuleSchema = new Schema({
-    serial: String,
-    type: String,
-    waterLandings: Number
-})
-
-
-const Capsule = mongoose.model('Capsule', capsuleSchema)
-=======
 const mongoose = require('mongoose');
 // Schema class from mongoose
 const Schema = mongoose.Schema;
@@ -25,6 +11,5 @@ const capsuleSchema = new Schema({
 
 // Model
 const Capsule = mongoose.model('Capsule', capsuleSchema);
->>>>>>> d80a148f58f87399c183d479e75d7b59ffaafd5b
 
 module.exports = Capsule;
--- models/index.js
+++ models/index.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require('mongoose')
-
-const config = {
-    userNewURLParser: true
-}
-mongoose.connect('mongodb://localhost/gaSpaceX' , {
-    userNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTechnology: true,
-=======
 const mongoose = require('mongoose');
 
 // options (object)
@@ -23,28 +12,18 @@ mongoose.connect('mongodb://localhost/gaSpaceX', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
->>>>>>> d80a148f58f87399c183d479e75d7b59ffaafd5b
     useFindAndModify: false
 });
 
 const db = mongoose.connection;
 
 db.once('open', () => {
-<<<<<<< HEAD
-    console.log(`Connected to MongoDB at ${db.host}: ${db.port}`)
-})
-
-db.on('error', (err) => {
-    console.log(`=========> ERROR`)
-    console.log(err)
-=======
     console.log(`Connected to MongoDB at ${db.host}: ${db.port}`);
 });
 
 db.on('error', (err) => {
     console.log('======> ERROR');
     console.log(err);
->>>>>>> d80a148f58f87399c183d479e75d7b59ffaafd5b
 });
 
 
